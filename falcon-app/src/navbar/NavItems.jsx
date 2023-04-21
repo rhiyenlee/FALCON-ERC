28019ed1507a868aebc383c93639241355aa96bb
jajaphernelia <jspimentel2020@plm.edu.ph>
2023-04-21 17:32:40 +0800

    final navbar for visitors

--- falcon-app/src/navbar/NavItems.jsx
+++ falcon-app/src/navbar/NavItems.jsx
@@ -1,22 +1,22 @@
 export const navItems = [
-    {
-      title: "Home",
-      path: "/home",
-      cName: "nav-item",
-    },
-    {
-      title: "Services",
-      path: "/services",
-      cName: "nav-item",
-    },
-    {
-      title: "About Us",
-      path: "/aboutus",
-      cName: "nav-item",
-    },
-    {
-      title: "Contact",
-      path: "/contact",
-      cName: "nav-item",
-    },
-  ];
+  {
+    title: "Home",
+    path: "/home",
+    cName: "nav-item",
+  },
+  {
+    title: "Services",
+    path: "/services",
+    cName: "nav-item",
+  },
+  {
+    title: "About Us",
+    path: "/aboutus",
+    cName: "nav-item",
+  },
+  {
+    title: "Contact",
+    path: "/contact",
+    cName: "nav-item",
+  },
+];
--- falcon-app/src/navbar/navbar.jsx
+++ falcon-app/src/navbar/navbar.jsx
@@ -1,30 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.css";
 import "./style/navbar.css";
 import Logo from "../Images/falcon-logo.png";
 import { navItems } from "./NavItems";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faX } from "@fortawesome/free-solid-svg-icons";
 
-export default function navbar() {
+export default function Navbar() {
+  const [Mobile, setMobile] = useState(false);
+
   return (
-    <nav className="navbar">
+    <nav className="navi">
       <div className="color-container"></div>
       <div className="main-navbar">
         <img src={Logo} alt="Logo" className="logo-png" />
-        <h1 className="falcon">FALCon</h1>
-        <ul className="nav-items">
-        {navItems.map((item) => {
-          return (
-            <>
-              <li key={item.id} className={item.cName}>
-                <NavLink to={item.path}>{item.title}</NavLink>
-              </li>
-            </>
-          );
-        })}
-      </ul>
-      <a href="#"className="enroll_btn">ENROLL</a>
-      <a href="#"className="login_btn">LOG IN</a>
+        <h1 className="falcon-title">FALCon</h1>
+        <ul
+          className={Mobile ? "Navlinks_Mobile" : "nav-items"}
+          onClick={() => setMobile()}
+        >
+          {navItems.map((item, index) => {
+            return (
+              <>
+                <li key={index} className={item.cName}>
+                  <NavLink to={item.path}>{item.title}</NavLink>
+                </li>
+              </>
+            );
+          })}
+          <div className="enroll">
+            <Link to="/enrollment" className="enroll_btn">
+              <button type="button" class="btn btn-success">
+                ENROLL
+              </button>
+            </Link>
+
+            <Link to="/login" className="login_btn">
+              <button type="button" class="btn btn-success">
+                LOGIN
+              </button>
+            </Link>
+          </div>
+        </ul>
+
+        {/**Mobile Navigation */}
+        <div className="Mobile_Navigation">
+          <a
+            className="Mobile_Navigation_Button"
+            onClick={() => setMobile(!Mobile)}
+          >
+            {Mobile ? (
+              <FontAwesomeIcon icon={faX} size="2xl" />
+            ) : (
+              <FontAwesomeIcon icon={faBars} size="2xl" />
+            )}
+          </a>
+        </div>
       </div>
     </nav>
   );
-} 
+}

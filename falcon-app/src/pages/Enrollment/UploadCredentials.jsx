1159bf208ad66ffb085459d63a13045717eedc67
0ojireho0 <101253324+0ojireho0@users.noreply.github.com>
2023-04-24 18:01:47 +0800

    done enrollment

--- falcon-app/src/pages/Enrollment/UploadCredentials.jsx
+++ falcon-app/src/pages/Enrollment/UploadCredentials.jsx
@@ -1,13 +1,6 @@
-import { useStepperContext } from "./context/StepperContext";
 import "./style/enrollment.css";
 
 export default function UploadCredentials() {
-  const { userData, setUserData } = useStepperContext();
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setUserData({ ...userData, [name]: value });
-  };
   return (
     <div className="upload-credentials-container">
       <h3 className="upload-documents">Upload the Documents</h3>
@@ -20,8 +13,6 @@ export default function UploadCredentials() {
           <input
             type="file"
             className="file-latin-honors relative block min-w-0 rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:rounded-md file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:file:bg-neutral-700 dark:file:text-neutral-100 dark:focus:border-primary "
-            onChange={handleChange}
-            value={userData["username"] || ""}
           />
         </div>
         <div className="id-card-container">

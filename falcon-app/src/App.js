ef3decd5d2bf02779d01ac97238456e3954f1c68
0ojireho0 <101253324+0ojireho0@users.noreply.github.com>
2023-04-24 18:34:57 +0800

    dsadsa

--- falcon-app/src/App.js
+++ falcon-app/src/App.js
@@ -1,17 +1,32 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./navbar/navbar";
-import { BrowserRouter } from "react-router-dom";
-import Services from "./pages/Services/services";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Enrollment from "./pages/Enrollment/enrollment";
+import Home from "./pages/Home/home";
+import ViewProfile from "./pages/ViewProfile/viewprofile";
+import Aboutus from "./pages/AboutUs/aboutus";
+import ForgotPassword from "./pages/ForgotPassword/forgotpassword";
+import Login from "./pages/Login/login";
+import ResetPassword from "./pages/ResetPassword/resetpassword";
+
 
 function App() {
   return (
     <>
       <BrowserRouter>
-      <Navbar />
+        <Navbar />
+        <Routes>
+          <Route path="/enrollment" element={<Enrollment />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/viewprofile" element={<ViewProfile />} />
+          <Route path="/aboutus" element={<Aboutus />} />
+          <Route path="/forgotpassword" element={<ForgotPassword />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/resetpassword" element={<ResetPassword />} />
+        </Routes>
       </BrowserRouter>
-      <Services />
-  
     </>
   );
 }
